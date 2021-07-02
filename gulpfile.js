92d51f6d6f37afbbfe88689e32a90121140b6a9f
Vadim Petrashenko <vadimp@compatibl.com>
2021-07-02 17:26:57 +0300

    added styles task for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,20 @@
-const gulp = require("gulp");
+const {task, src, dest} = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const gulpIf = require('gulp-if');
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const concat = require("gulp-concat");
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+task("styles", () => src("src/styles/**/*.scss")
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(concat('index.min.css'))
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(dest('dist'))
+    // .pipe(browserSync.stream())
+);
